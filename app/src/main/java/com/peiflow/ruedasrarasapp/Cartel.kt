7326ab9c53e3c685cd52cdaad3f5cceffd470923
peiflow <palvarezg.work@outlook.es>
2019-06-16 02:26:18 +0200

    chrisbanes/PhotoView implemented

--- app/src/main/java/com/peiflow/ruedasrarasapp/Cartel.kt
+++ app/src/main/java/com/peiflow/ruedasrarasapp/Cartel.kt
@@ -2,11 +2,14 @@ package com.peiflow.ruedasrarasapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cartel.*
 
 class Cartel : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cartel)
+
+        pv_cartel.setImageResource(R.drawable.cartel_concencentracion)
     }
 }
